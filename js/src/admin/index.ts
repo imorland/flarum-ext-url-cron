da969cc3e1aaa7a13cc8e0f1f6cd845cdfa46805
Ian Morland <ian@morland.me>
2023-05-10 09:28:46 +0100

    chore: prettier

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,12 +1,11 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('ianm-url-cron', () => {
-    app.extensionData.for('ianm-url-cron')
-        .registerSetting({
-            setting: 'ianm-url-cron.php-path',
-            label: app.translator.trans('ianm-url-cron.admin.settings.php-path'),
-            help: app.translator.trans('ianm-url-cron.admin.settings.php-path-help'),
-            type: 'text',
-            placeholder: '/usr/bin/php',
-        })
+  app.extensionData.for('ianm-url-cron').registerSetting({
+    setting: 'ianm-url-cron.php-path',
+    label: app.translator.trans('ianm-url-cron.admin.settings.php-path'),
+    help: app.translator.trans('ianm-url-cron.admin.settings.php-path-help'),
+    type: 'text',
+    placeholder: '/usr/bin/php',
+  });
 });
